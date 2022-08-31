f0da97d843811869556887a2dabc27af6fa7ceb0
NagendraProjects <112543293+NagendraProjects@users.noreply.github.com>
2022-08-31 14:32:06 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,21 +1,52 @@
-import numpy as np
-from flask import Flask, request, jsonify, render_template
+# coding: utf-8
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier 
+from sklearn import metrics
+from flask import Flask, request, render_template
+import re
+import math
 import pickle
 
-# Create flask app
-flask_app = Flask(__name__)
-model = pickle.load(open('xgboost_tuned.sav', 'rb'))
+app = Flask("__name__")
 
-@flask_app.route("/")
-def Home():
-    return render_template("index.html")
+q = ""
 
-@flask_app.route("/predict", methods = ["POST"])
+@app.route("/")
+def loadPage():
+	return render_template('home.html', query="")
+
+
+
+@app.route("/predict", methods=['POST'])
 def predict():
-    float_features = [float(x) for x in request.form.values()]
-    features = [np.array(float_features)]
-    prediction = model.predict(features)
-    return render_template("index.html", prediction_text = "The flower species is {}".format(prediction))
+    
+    model = pickle.load(open("xgboost_tuned.sav", "rb"))
+    
 
+    inputQuery1 = request.form['query1']
+    inputQuery2 = request.form['query2']
+    inputQuery3 = request.form['query3']
+    inputQuery4 = request.form['query4']
+    inputQuery5 = request.form['query5']
+    inputQuery6 = request.form['query6']
+    inputQuery7 = request.form['query7']
+    inputQuery8 = request.form['query8']
+
+    
+    
+    
+    
+    data = [[inputQuery1, inputQuery2, inputQuery3, inputQuery4, inputQuery5, inputQuery6, inputQuery7, inputQuery8]]
+    
+    # Create the pandas DataFrame 
+    new_df = pd.DataFrame(data, columns = ['Week No', 'Year', 'Mortgage Rate', 'Treasury Yield','Unemployment Rate', 'GDP', 'Consumer Confidence Index','Disposable Income'])
+    prediction = model.predict(new_df)
+    o1 = "The forecasting value is {}".format(prediction)
+    
+    return render_template('home.html', output1=o1, query1 = request.form['query1'], query2 = request.form['query2'],query3 = request.form['query3'],query4 = request.form['query4'],
+                           query5 = request.form['query5'],query6 = request.form['query6'],query7 = request.form['query7'],query8 = request.form['query8'])
+    
 if __name__ == "__main__":
-    flask_app.run(debug=True)
+    app.run(debug=True)
