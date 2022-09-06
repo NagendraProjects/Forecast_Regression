47ee4cb94d0d89f8570fc8c02fcebacbb8f60dbd
NagendraProjects <112543293+NagendraProjects@users.noreply.github.com>
2022-09-06 10:01:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+import warnings
+warnings.filterwarnings('ignore',category=FutureWarning)
 import pandas as pd
 #from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor 
@@ -12,7 +14,17 @@ import pickle
 
 app = Flask("__name__")
 
-q = ""
+d1=pd.read_excel('Weekly data Refinance Volumes_upto_aug1.xlsx')
+future_data  = d1[d1['Refinance'].isna()]
+base_data = d1[~d1['Refinance'].isna()]
+x=base_data[['Year','Mortgage Rate','Inflation']]
+y=base_data['Refinance']
+
+X_train,X_test,y_train,y_test=train_test_split(x,y,test_size=0.10,random_state=24)
+model = RandomForestRegressor(n_estimators=500,criterion='mae',max_features='auto', max_depth=2,min_samples_split=2,min_samples_leaf=4,bootstrap=True,n_jobs=-1, random_state=102)
+model.fit(X_train, y_train)
+print("Train Accuracy:",model.score(X_train, y_train))
+print("Test Accuracy:",model.score(X_test, y_test))
 
 @app.route("/")
 def loadPage():
@@ -21,33 +33,22 @@ def loadPage():
 
 
 @app.route("/predict", methods=['POST'])
-def predict():
+def predict():    
+    #model = pickle.load(open("RF_tuned.sav", "rb"))
     
-    model = pickle.load(open("RF_tuned.sav", "rb"))
-    
-
     inputQuery1 = request.form['query1']
     inputQuery2 = request.form['query2']
-    inputQuery3 = request.form['query3']
-    inputQuery4 = request.form['query4']
-    inputQuery5 = request.form['query5']
-    inputQuery6 = request.form['query6']
-    inputQuery7 = request.form['query7']
-    inputQuery8 = request.form['query8']
-
+    inputQuery3 = request.form['query3']    
+       
     
-    
-    
-    
-    data = [[inputQuery1, inputQuery2, inputQuery3, inputQuery4, inputQuery5, inputQuery6, inputQuery7, inputQuery8]]
+    data = [[inputQuery1, inputQuery2, inputQuery3]]
     
     # Create the pandas DataFrame 
-    new_df = pd.DataFrame(data, columns = ['Week No', 'Year', 'Mortgage Rate', 'Treasury Yield','Unemployment Rate', 'GDP', 'Consumer Confidence Index','Disposable Income'])
+    new_df = pd.DataFrame(data, columns = ['Year', 'Mortgage Rate', 'Inflation'])
     prediction = model.predict(new_df)
     o1 = "The forecasting value is {}".format(prediction)
     
-    return render_template('home.html', output1=o1, query1 = request.form['query1'], query2 = request.form['query2'],query3 = request.form['query3'],query4 = request.form['query4'],
-                           query5 = request.form['query5'],query6 = request.form['query6'],query7 = request.form['query7'],query8 = request.form['query8'])
+    return render_template('home.html', output1=o1, query1 = request.form['query1'], query2 = request.form['query2'],query3 = request.form['query3'])
     
 if __name__ == "__main__":
     app.run(debug=True)
